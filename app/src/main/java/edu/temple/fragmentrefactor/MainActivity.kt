1cccdb788f469f369d4417645a5ed050a01961b3
Addison Migash <tuo20482@temple.edu>
2023-10-23 16:34:17 -0400

    Added fragment logic to MainActivity.kt

--- app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainActivity.kt
@@ -1,28 +1,18 @@
 package edu.temple.fragmentrefactor
 
-import androidx.appcompat.app.AppCompatActivity
+import NameFragment
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val displayTextView = findViewById<TextView>(R.id.displayTextView)
-        val nameEditText = findViewById<EditText>(R.id.nameEditText)
-        val changeButton = findViewById<Button>(R.id.changeButton)
-
-        changeButton.setOnClickListener {
-            val name = nameEditText.text
-
-            displayTextView.text = if (name.isNotBlank()) {
-                "Hello, $name!"
-            } else {
-                "Please enter your name"
-            }
+        if (savedInstanceState == null) {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.fragmentContainer, NameFragment())
+            fragmentTransaction.commit()
         }
     }
 }
